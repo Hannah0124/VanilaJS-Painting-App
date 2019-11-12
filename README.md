cb1da83f0d84f97c7a92b750011cefdab84ff7b6
Hannah0124 <hannahjoo24@gmail.com>
2019-11-12 14:46:24 -0800

    Cursor Update

--- README.md
+++ README.md
@@ -11,3 +11,4 @@ Nomad Coders: https://academy.nomadcoders.co/p/vanilla-js-part-two-kr
 * CSS Tools - Reset CSS: https://meyerweb.com/eric/tools/css/reset/
 * MDN - CanvasRenderingContext2D: https://developer.mozilla.org/en-US/docs/Web/API/CanvasRenderingContext2D
 * MDN - HTMLCanvasElement.toDataURL(): https://developer.mozilla.org/en-US/docs/Web/API/HTMLCanvasElement/toDataURL
+* Paint Bucket Icon: Icons made by <a href="https://www.flaticon.com/authors/freepik" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a></div>
--- app.js
+++ app.js
@@ -1,20 +1,20 @@
-const canvas = document.getElementById("jsCanvas");
+const canvas = document.getElementById('jsCanvas');
 const ctx = canvas.getContext('2d');
-const colors = document.getElementsByClassName("jsColor");
+const colors = document.getElementsByClassName('jsColor');
 const range = document.getElementById('jsRange');
 const mode = document.getElementById('jsMode');
 const saveBtn = document.getElementById('jsSave');
 
-const INITIAL_COLOR = "#2c2c2c";
+const INITIAL_COLOR = '#2c2c2c';
 const CANVAS_SIZE = 500;
 
 // Update the pixel of the canvas
 canvas.width = CANVAS_SIZE;
 canvas.height = CANVAS_SIZE;
 
-// Set the default of the context 
-ctx.fillStyle = "white";
-ctx.fillRect(0, 0, canvas.width, canvas.height); 
+// Set the default of the context
+ctx.fillStyle = 'white';
+ctx.fillRect(0, 0, canvas.width, canvas.height);
 ctx.strokeStyle = INITIAL_COLOR;
 ctx.fillStyle = INITIAL_COLOR;
 ctx.lineWidth = 5.0;
@@ -23,92 +23,99 @@ let painting = false;
 let filling = false;
 
 function stopPainting() {
-  painting = false;
+	painting = false;
 }
 
 function startPainting() {
-  painting = true;
-};
+	painting = true;
+	if (painting === true) {
+		canvas.classList.add('brush');
+	}
+}
 
 function onMouseMove(event) {
-  const x = event.offsetX;
-  const y = event.offsetY; 
-  // When I don't paint..
-  if(!painting) {
-    ctx.beginPath();
-    ctx.moveTo(x, y);
-  // When painting..
-  } else {
-    ctx.lineTo(x, y);
-    ctx.stroke();
-  }
-};
-
+	const x = event.offsetX;
+	const y = event.offsetY;
+	// When I don't paint..
+	if (!painting) {
+		ctx.beginPath();
+		ctx.moveTo(x, y);
+		// When painting..
+	} else {
+		ctx.lineTo(x, y);
+		ctx.stroke();
+	}
+}
 
 function handleColorClick(event) {
-  const color = event.target.style.backgroundColor;
-  ctx.strokeStyle = color;
-  ctx.fillStyle = color;
-};
+	const color = event.target.style.backgroundColor;
+	ctx.strokeStyle = color;
+	ctx.fillStyle = color;
+}
 
 function handleRangeChange(event) {
-  const size = event.target.value;
-  ctx.lineWidth = size;
+	const size = event.target.value;
+	ctx.lineWidth = size;
 }
 
 function handleModeClick() {
-  // Paint (brush) mode
-  if(filling === true) {
-    filling = false;
-    mode.innerText = "Fill";
-    // Filling mode
-  } else {
-    filling = true;
-    mode.innerText = "Paint";
-  }
+	// Paint (brush) mode
+	if (filling === true) {
+		canvas.classList.add('brush');
+		canvas.classList.remove('paintBucket');
+		filling = false;
+		mode.innerText = 'Fill';
+		// Filling mode
+	} else {
+		canvas.classList.remove('brush');
+		canvas.classList.add('paintBucket');
+		filling = true;
+		mode.innerText = 'Paint';
+	}
 }
 
 function handleCanvasClick() {
-  if (filling) {
-    ctx.fillRect(0, 0, canvas.width, canvas.height); // .fillRect(x, y, width, height)
-  }
-};
+	if (filling) {
+		canvas.classList.add('paintBucket');
+		canvas.classList.remove('brush');
+		ctx.fillRect(0, 0, canvas.width, canvas.height); // .fillRect(x, y, width, height)
+	}
+}
 
 // Handle Context Menu
 function handleCM(event) {
-  event.preventDefault();
-};
+	event.preventDefault();
+}
 
 function handleSaveClick() {
-  const image = canvas.toDataURL();
-  const link = document.createElement('a');
-  link.href = image;
-  link.download = "PaintJS[🎨]"; 
-  link.click();
-};
+	const image = canvas.toDataURL();
+	const link = document.createElement('a');
+	link.href = image;
+	link.download = 'PaintJS[🎨]';
+	link.click();
+}
 
+if (canvas) {
+	canvas.addEventListener('mousemove', onMouseMove);
+	// Mouse click
+	canvas.addEventListener('mousedown', startPainting);
+	// Mouse unclick
+	canvas.addEventListener('mouseup', stopPainting);
+	canvas.addEventListener('mouseleave', stopPainting);
+	canvas.addEventListener('click', handleCanvasClick);
+	canvas.addEventListener('contextmenu', handleCM); // Mouse right click
+}
 
-if(canvas) {
-  canvas.addEventListener('mousemove', onMouseMove);
-  // Mouse click
-  canvas.addEventListener('mousedown', startPainting);
-  // Mouse unclick
-  canvas.addEventListener('mouseup', stopPainting);
-  canvas.addEventListener('mouseleave', stopPainting);
-  canvas.addEventListener('click', handleCanvasClick);
-  canvas.addEventListener('contextmenu', handleCM); // Mouse right click
-};
+Array.from(colors).forEach((color) => color.addEventListener('click', handleColorClick));
 
-Array.from(colors).forEach(color => color.addEventListener("click", handleColorClick));
+if (range) {
+	range.addEventListener('input', handleRangeChange);
+}
 
-if(range) {
-  range.addEventListener('input', handleRangeChange);
-};
+if (mode) {
+	mode.addEventListener('click', handleModeClick);
+}
 
-if(mode) {
-  mode.addEventListener('click', handleModeClick);
-};
-
-if(saveBtn) {
-  saveBtn.addEventListener('click', handleSaveClick);
+if (saveBtn) {
+	saveBtn.addEventListener('click', handleSaveClick);
 }
