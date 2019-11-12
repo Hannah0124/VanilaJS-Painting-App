54eed6db15a3e4a42d358391aa11c05dc460d724
Hannah0124 <hannahjoo24@gmail.com>
2019-11-11 21:47:07 -0800

    Changing Color

--- README.md
+++ README.md
@@ -3,8 +3,7 @@ Painting board made with VanilaJS
 
 ## Built With
 * [Vanila JS](http://vanilla-js.com/) 
-* [OpenWeather API](https://openweathermap.org/api) - Simple and fast and free weather API from OpenWeatherMap
 
 ## Resources
-CSS Tools - Reset CSS: https://meyerweb.com/eric/tools/css/reset/
-MDN: https://developer.mozilla.org/en-US/docs/Web/API/CanvasRenderingContext2D
+* CSS Tools - Reset CSS: https://meyerweb.com/eric/tools/css/reset/
+* MDN - CanvasRenderingContext2D: https://developer.mozilla.org/en-US/docs/Web/API/CanvasRenderingContext2D
--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const canvas = document.getElementById("jsCanvas");
 const ctx = canvas.getContext('2d');
+const colors = document.getElementsByClassName("jsColor")
 
 // Update the pixel of the canvas
 canvas.width = 500;
@@ -35,9 +36,11 @@ function onMouseMove(event) {
   }
 };
 
-function onMouseDown(event) {
-  painting = true;
-}
+
+function handleColorClick(event) {
+  const color = event.target.style.backgroundColor;
+  ctx.strokeStyle = color;
+};
 
 if(canvas) {
   canvas.addEventListener('mousemove', onMouseMove);
@@ -47,3 +50,5 @@ if(canvas) {
   canvas.addEventListener('mouseup', stopPainting);
   canvas.addEventListener('mouseleave', stopPainting);
 }
+
+Array.from(colors).forEach(color => color.addEventListener("click", handleColorClick));
